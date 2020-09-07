d5596e3b71af2516e4652ffdf101117b4c6f779b
Xiaochaxixue <18290108763@163.com>
2020-09-07 11:57:52 +0800

    排针工艺与天线工艺具体实现方法
    将排针工艺方法与天线工艺具体方法函数框架给实现了

--- src/utils/ShowCraftInfo.java
+++ src/utils/ShowCraftInfo.java
@@ -16,9 +16,42 @@ public class ShowCraftInfo {
 	 * @param pt
 	 * @return
 	 */
+	String pinNum;//排针数量
+	String pinSize;//排针大小
+	String pinShape;//排针形状
+	String pinWeld;//焊接方式
+	
+	String antennaType;//天线类型
+	String antennaLength;//天线长度
+	
+	List<String> craftInfoLists = new ArrayList<String>();
 	public List<String> getDetailCraftInfoByPt(String pt){
-		List<String> craftInfoLists = new ArrayList<String>();
-		
+		//List<String> craftInfoLists = new ArrayList<String>();
+		String Pin;//排针工艺
+		String Antenna;//天线工艺
+		/**
+		 * 将工艺具体到哪个工艺
+		 * 排针工艺还是天线工艺
+		 */
+		if(pt.contains("P")||pt.contains("B")||pt.contains("C")||pt.contains("I")){
+			/**
+			 * 判断是否含有工艺技术
+			 * 含有的话进行处理
+			 * 没有的话则跳出来
+			 */
+			if(pt.contains("P")&&(pt.contains("B")||pt.contains("C")||pt.contains("I"))){
+				/**
+				 * 既有排针工艺又有天线工艺
+				 */
+				String[] pinAndantenna =pt.split("-");
+				Pin = pinAndantenna[0];
+				Antenna = pinAndantenna[1];
+				handlePin(Pin);
+				handleAntenna(Antenna);
+			}
+		}else{
+			return null;
+		}
 		return craftInfoLists;
 	}
 	/**
@@ -31,4 +64,22 @@ public class ShowCraftInfo {
 	 * 						B表示：标准铁氟龙天线
 	 * 						3表示：天线长度为3cm
 	 */
+	/**
+	 * 方法说明：处理排针工艺方法
+	 * @param pin
+	 * @return
+	 */
+	public void handlePin(String pin){
+		
+		return;
+	}
+	/**
+	 * 方法说明：处理天线工艺方法
+	 * @param antenna
+	 * @return
+	 */
+	public void handleAntenna(String antenna){
+		
+		return;
+	}
 }
