f6dec075b22cf861c8f00bb334ce9f6c2a92d351
Xiaochaxixue <18290108763@163.com>
2020-09-07 15:07:59 +0800

    工艺工具类完成

--- src/utils/ShowCraftInfo.java
+++ src/utils/ShowCraftInfo.java
@@ -1,7 +1,7 @@
 package utils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShowCraftInfo {
 	/**
@@ -24,8 +24,8 @@ public class ShowCraftInfo {
 	String antennaType;//天线类型
 	String antennaLength;//天线长度
 	
-	List<String> craftInfoLists = new ArrayList<String>();
-	public List<String> getDetailCraftInfoByPt(String pt){
+	Map<String,String> craftInfoLists = new HashMap<String,String>();
+	public Map<String,String> getDetailCraftInfoByPt(String pt){
 		//List<String> craftInfoLists = new ArrayList<String>();
 		String Pin;//排针工艺
 		String Antenna;//天线工艺
@@ -48,6 +48,16 @@ public class ShowCraftInfo {
 				Antenna = pinAndantenna[1];
 				handlePin(Pin);
 				handleAntenna(Antenna);
+			}else if(pt.contains("P")&&!(pt.contains("B")||pt.contains("C")||pt.contains("I"))){
+				/**
+				 * 有排针工艺没有天线工艺
+				 */
+				handlePin(pt);
+			}else if(!pt.contains("P")&&(pt.contains("B")||pt.contains("C")||pt.contains("I"))){
+				/**
+				 * 有天线工艺没有排针工艺
+				 */
+				handlePin(pt);
 			}
 		}else{
 			return null;
@@ -59,7 +69,7 @@ public class ShowCraftInfo {
 	 * 排针说明：eg：3P2.54WZ
 	 * 						3P表示：排针数量为3
 	 * 						2.54：排针间距为2.54
-	 * 						WZ:排针形状为直排针，焊接方式为正焊
+	 * 						WZ:排针形状为弯排针，焊接方式为正焊
 	 * 天线说明：eg：B3
 	 * 						B表示：标准铁氟龙天线
 	 * 						3表示：天线长度为3cm
@@ -69,7 +79,28 @@ public class ShowCraftInfo {
 	 * @param pin
 	 * @return
 	 */
-	public void handlePin(String pin){
+	private void handlePin(String pin){
+		/**eg:3P2.54WZ
+		 * 排针数量String pinNum
+		 * 排针大小String pinSize
+		 * 排针形状String pinShape
+		 * 焊接方式String pinWeld
+		 */
+		int pLocate;
+		pLocate = pin.indexOf("P");
+		pinNum = pin.substring(0,pLocate);
+		craftInfoLists.put("pinNum",pinNum);
+		
+		int allLength;
+		allLength = pin.length();
+		pinSize = pin.substring(pLocate+1,allLength-1);
+		craftInfoLists.put("pinSize",pinSize);
+		
+		pinShape = ""+pin.charAt(allLength-1);
+		craftInfoLists.put("pinShape",pinShape);
+		
+		pinWeld = ""+pin.charAt(allLength);
+		craftInfoLists.put("pinWeld",pinWeld);
 		
 		return;
 	}
@@ -78,8 +109,16 @@ public class ShowCraftInfo {
 	 * @param antenna
 	 * @return
 	 */
-	public void handleAntenna(String antenna){
+	private void handleAntenna(String antenna){
+		/**eg:C18
+		 * 天线类型String antennaType
+		 * 天线长度String antennaLength
+		 */
+		antennaType = ""+antenna.charAt(0);
+		craftInfoLists.put("antennaType", antennaType);
 		
+		antennaLength = antenna.substring(1);
+		craftInfoLists.put("antennaLength", antennaLength);
 		return;
 	}
 }
